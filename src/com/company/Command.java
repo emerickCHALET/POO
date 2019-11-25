80cdad4c8ebb04235906566a82b5529e603ab320
Lexo <florentdebuchy@orange.fr>
2019-11-25 17:12:21 +0100

    ajout command et correction

--- src/com/company/Command.java
+++ src/com/company/Command.java
@@ -1,31 +1,36 @@
 package com.company;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.Scanner;
 
 public class Command
 {
-    public static Scanner sc = new Scanner(System.in);
-    public static void getUserChoice()
-    {
-        String tabCommand[] = {"Help","Exit","Create","Delete" };
-        do {
-            String choice = sc.nextLine();
-            if(choice.equals("Help"))
-            {
-                System.out.println(tabCommand);
 
-            }
-            if(choice.equals("Exit"))
+
+
+    static public String getUserChoice()
+    {
+        Scanner sc = new Scanner(System.in);
+        String tabCommand[] = {"Help","Exit","Create","Delete" };
+        String choice;
+        Boolean BOOOOOL = false;
+        choice = sc.nextLine();
+        for(int i = 0; i < tabCommand.length; i++)
+        {
+            if(choice.equals(tabCommand[i]))
             {
-                System.out.println("Bye bye");
-                System.exit(0);
+                BOOOOOL = true;
             }
-            else
-            {
-                System.out.println("Error");
-                System.out.println(tabCommand);
-            }
-        }while(true);
+        }
+        if(BOOOOOL == false)
+        {
+            //help
+            System.out.println("DebugError");
+            choice = "Help";
+        }
+        return choice;
     }
+
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,18 @@ public class Main {
      * args significate arguments, This table is the mechanism for the system to pass the information to your application
      * @param args
      */
-    public static void main(String[] args) {
-        System.out.println("Application has started");
+    public static void main(String[] args)
+    {
+        execution();
     }
+    public static void execution()
+    {
+        System.out.println("Application has started");
+        String ch;
+        do {
+            ch = Command.getUserChoice();
+
+        }while(!ch.equals("Exit"));
+    }
+    
 }
