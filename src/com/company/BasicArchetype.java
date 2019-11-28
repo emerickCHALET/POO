0f4afd64e1c999eacfbd1273ff91eecbcf019fb7
marine <marine.b.58@hotmail.fr>
2019-11-28 18:18:24 +0100

    story 3 without diagram class

--- src/com/company/BasicArchetype.java
+++ src/com/company/BasicArchetype.java
@@ -1,9 +1,66 @@
-/*package com.company;
+package com.company;
 
 public class BasicArchetype extends Personnage
 {
-    public BasicArchetype()
+    /**
+     * Constructor of Basic archetype
+     * @param pBasic all information of personnage
+     */
+    public BasicArchetype(Personnage pBasic)
+    {
+        super(pBasic.getName(),pBasic.getHp(),pBasic.getDamage(),pBasic.getInitiative());
+    }
+    /** Return of the name of the character*/
+    public String getName()
+    {
+        return (super.getName());
+    }
+    /** Return of the points of life of the character*/
+    public int getHp()
+    {
+        return (super.getHp());
+    }
+    /** Return of the points that the character undergoes */
+    public int getDamage()
+    {
+        return super.getDamage();
+    }
+    /** Return of the points of initiative of the character*/
+    public int getInitiative()
+    {
+        return super.getInitiative();
+    }
+    /** Return the remaining life points of the character*/
+    public int hurt(int damage)
+    {
+        int res;
+        res = super.getHp() - damage;
+        return res;
+    }
+
+    /**
+     * return class of personnage
+     * @return null
+     */
+    public String getClassA()
+    {
+        return null;
+    }
+
+    /**
+     * return of element of character
+     * @return element of the character
+     */
+    public String toString()
+    {
+        return (super.toString());
+    }
+
+    /**
+     * Method for start the fight
+     */
+    public static void startFight()
     {
 
     }
-}*/
+}
